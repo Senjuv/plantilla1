2b1d9e5ff2d8f797956a3d5d3d02af223be71d21
Senju1615 <170354763+Senjuv@users.noreply.github.com>
2026-07-17 23:05:44 -0600

    Version1

--- src/pages/productos.jsx
+++ src/pages/productos.jsx
@@ -19,48 +19,40 @@ function Productos() {
                     <div className="div-p">
                         <Link to="/product" ><img src={oso} alt="Producto1" className='img-p' /></Link>
                         <div className="infoProduct">
-                            <div>
                                 <h2 className="InfoName">Osito</h2>
-                            </div>
-                            <div>
                                 <h2 className="InfoName">150$</h2>
-                            </div>
                         </div>
                     </div>
-                    <div>
+                    <div className="div-p">
                         <Link to="/product" ><img src={lavanda2} alt="Producto1" className='img-p' /></Link>
                         <div className="infoProduct">
-                            <div>
-                                <h2 className="InfoName">Lavanda con chia</h2>
-                            </div>
-                            <div>
-                                <h2 className="InfoName">100$</h2>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <Link to="/product" ><img src={verde} alt="Producto1" className='img-p' /></Link>
-                        <div className="infoProduct">
-                            <h2 className="InfoName">Limon con fresa</h2>
+                            <h2 className="InfoName">Lavanda con chia</h2>
                             <h2 className="InfoName">100$</h2>
                         </div>
                     </div>
+                    <div className="div-p">
+                        <Link to="/product" ><img src={verde} alt="Producto1" className='img-p' /></Link>
+                        <div className="infoProduct">
+                            <h2 className="InfoName">Verde</h2>
+                            <h2 className="InfoName">150$</h2>
+                        </div>
+                    </div>
 
-                    <div>
+                    <div className="div-p">
                         <Link to="/product" ><img src={cuatro} alt="Producto1" className='img-p' /></Link>
                         <div className="infoProduct">
                             <h2 className="InfoName">Luma</h2>
                             <h2 className="InfoName">150$</h2>
                         </div>
                     </div>
-                    <div>
+                    <div className="div-p">
                         <Link to="/product" ><img src={capuccino} alt="Producto1" className='img-p' /></Link>
                         <div className="infoProduct">
                             <h2 className="InfoName">Capuccino</h2>
                             <h2 className="InfoName">100$</h2>
                         </div>
                     </div>
-                    <div>
+                    <div className="div-p">
                         <Link to="/product" ><img src={mariposa} alt="Producto1" className='img-p' /></Link>
                         <div className="infoProduct">
                             <h2 className="InfoName">Mariposita</h2>
